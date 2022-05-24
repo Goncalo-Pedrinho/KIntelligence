8c55adf125c5cbe2a75389e26989e1728c3583bc
Gonçalo Pedrinho <pedrinho17065@gmail.com>
2022-05-24 11:13:16 +0100

    Introdução das definições

---
+++ definitions/script.js
@@ -0,0 +1,58 @@
+function OptSelect() {
+    document.getElementById("Opt1").style.backgroundColor = "#242526";
+    document.getElementById("Opt1").style.color = "white";
+
+    document.getElementById("Opt2").style.backgroundColor = "#dad8d8";
+    document.getElementById("Opt2").style.color = "black";
+
+    document.getElementById("Opt3").style.backgroundColor = "#dad8d8";
+    document.getElementById("Opt3").style.color = "black";
+}
+function OptSelect2() {
+    document.getElementById("Opt2").style.backgroundColor = "#242526";
+    document.getElementById("Opt2").style.color = "white";
+
+    document.getElementById("Opt1").style.backgroundColor = "#dad8d8";
+    document.getElementById("Opt1").style.color = "black";
+    document.getElementById("Opt3").style.backgroundColor = "#dad8d8";
+    document.getElementById("Opt3").style.color = "black";
+}
+function OptSelect3() {
+    document.getElementById("Opt3").style.backgroundColor = "#242526";
+    document.getElementById("Opt3").style.color = "white";
+
+
+    document.getElementById("Opt2").style.backgroundColor = "#dad8d8";
+    document.getElementById("Opt2").style.color = "black";
+    document.getElementById("Opt1").style.backgroundColor = "#dad8d8";
+    document.getElementById("Opt1").style.color = "black";
+}
+
+
+
+
+
+
+
+
+
+
+
+function Underline() {
+    document.getElementById("UL1").style.borderBottom = "2px solid black"
+
+    document.getElementById("UL2").style.borderBottom = "none";
+    document.getElementById("UL3").style.borderBottom = "none";
+}
+
+function Underline2() {
+    document.getElementById("UL2").style.borderBottom = "2px solid black"
+    document.getElementById("UL1").style.borderBottom = "none";
+    document.getElementById("UL3").style.borderBottom = "none";
+}
+
+function Underline3() {
+    document.getElementById("UL3").style.borderBottom = "2px solid black"
+    document.getElementById("UL2").style.borderBottom = "none";
+    document.getElementById("UL1").style.borderBottom = "none";
+}
